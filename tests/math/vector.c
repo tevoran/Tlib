0bc53381ad400edd38fdedf983874e83d0a2f2cc
tevoran <j.pfeiffer93@gmx.de>
2022-08-14 15:15:06 +0200

    changed tabs into spaces

--- tests/math/vector.c
+++ tests/math/vector.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <Tlib/math.h>
 
-#define TEST_VEC2(FUNCTION, A_X, A_Y, B_X, B_Y, D_X, D_Y)	\
-{															\
-	char string[100] = #FUNCTION;							\
-	printf("testing %s: ", string);							\
-	T_vec2 a,b,c,d;											\
-	a.x = A_X;												\
-	a.y = A_Y;												\
-	b.x = B_X;												\
-	b.y = B_Y;												\
-	d.x = D_X;												\
-	d.y = D_Y;												\
-	c = FUNCTION(a, b);										\
-	if(c.x == d.x && c.y == d.y)							\
-	{														\
-		printf("pass\n");									\
-	}														\
-	else													\
-	{														\
-		printf("fail\n");									\
-	}														\
+#define TEST_VEC2(FUNCTION, A_X, A_Y, B_X, B_Y, D_X, D_Y)   \
+{                                                           \
+    char string[100] = #FUNCTION;                           \
+    printf("testing %s: ", string);                         \
+    T_vec2 a,b,c,d;                                         \
+    a.x = A_X;                                              \
+    a.y = A_Y;                                              \
+    b.x = B_X;                                              \
+    b.y = B_Y;                                              \
+    d.x = D_X;                                              \
+    d.y = D_Y;                                              \
+    c = FUNCTION(a, b);                                     \
+    if(c.x == d.x && c.y == d.y)                            \
+    {                                                       \
+        printf("pass\n");                                   \
+    }                                                       \
+    else                                                    \
+    {                                                       \
+        printf("fail\n");                                   \
+    }                                                       \
 }
 
 #define TEST_VEC3(FUNCTION, A_X, A_Y, A_Z, B_X, B_Y, B_Z, D_X, D_Y, D_Z)    \
@@ -48,8 +48,8 @@
 
 int main(void)
 {
-	//vec2
-	TEST_VEC2(T_v2_add, 1.0f, 2.0f, 1.0f, 2.0f, 2.0f, 4.0f);
+    //vec2
+    TEST_VEC2(T_v2_add, 1.0f, 2.0f, 1.0f, 2.0f, 2.0f, 4.0f);
     TEST_VEC2(T_v2_sub, 1.0f, 2.0f, 1.0f, 2.0f, 0.0f, 0.0f);
-	return 0;
+    return 0;
 }

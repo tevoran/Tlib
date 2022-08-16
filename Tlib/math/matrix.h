d4c24664321db57d9815902353b4dcee39afc7da
tevoran <j.pfeiffer93@gmx.de>
2022-08-16 19:28:57 +0200

    adding matrix typedefs

--- Tlib/math/matrix.h
+++ Tlib/math/matrix.h
@@ -6,16 +6,16 @@
 struct T_mat2
 {
 	f32 array[2][2];
-};
+} typedef T_mat2;
 
 struct T_mat3
 {
 	f32 array[3][3];
-};
+} typedef T_mat3;
 
 struct T_mat4
 {
 	f32 array[4][4];
-};
+} typedef T_mat4;
 
 #endif /* TLIB_MATH_MATRIX */
